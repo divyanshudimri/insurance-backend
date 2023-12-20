d0faac366479b9a4c8c9cd220156c2c00d56fa34
Divyanshu Dimri <divyanshu.dimri01@gmail.com>
2023-12-20 18:30:57 +0530

    added users app, and changes for customer POST

---
+++ users/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'first_name', 'last_name', 'date_of_birth', 'username')
+        read_only_fields = ('id',)
